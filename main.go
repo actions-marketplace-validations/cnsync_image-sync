90f951d0b256b743edc78a17a6caf181fe94080b
cnsync <jyj.xyz@outlook.com>
2023-06-30 12:38:47 +0800

    init

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"sort"
 	"strings"
-	"sync"
 )
 
 var (
@@ -19,93 +18,42 @@ var (
 	finalTags []string
 )
 
-const MaxConcurrent = 5 // 最大并发数
-
-func main() {
-	body := httpclient("https://raw.githubusercontent.com/cnsync/image-sync/main/mirrors.txt")
-
-	SplitMirrors(body)
-
-	var wg sync.WaitGroup
-
-	semaphore := make(chan struct{}, MaxConcurrent)
-
-	for _, cmd := range mirrorCtx {
-		wg.Add(1)
-		semaphore <- struct{}{} // 申请一个信号量，控制并发数量
-
-		go func(command string) {
-			defer func() {
-				<-semaphore // 释放信号量
-				wg.Done()
-			}()
-
-			err := executeCommand(command)
-			if err != nil {
-				fmt.Printf("Error executing command %s: %s\n", command, err)
-			}
-		}(cmd)
-	}
-
-	wg.Wait()
-}
-
-func executeCommand(command string) error {
-
-	// 远程tag
-	remoteTags := listTags(command)
-
-	if remoteTags == nil {
-		fmt.Println("Empty tags for command:", command)
-		return nil
-	}
-
-	_, src, _ := ImageContains(remoteTags, "docker.io/cnxyz")
-
-	// 组装名称
-	//log.Println(dest, src)
-	// 获取本地仓库tag 去重
-	srcTags := listTags(src)
-
-	if srcTags != nil {
-		srcTags.Tags = finalTags
-	}
-
-	log.Println("----finalTags--------", finalTags)
-	log.Println("----remoteTags--------", remoteTags.Tags)
-
-	// 执行命令的逻辑
-	//fmt.Println("Executing command:", command)
-	//cmd := exec.Command("skopeo", "copy", "--insecure-policy", "--src-tls-verify=false", "--dest-tls-verify=false", "-q", "docker://"+src, "docker://"+dest)
-
-	//log.Printf("CMD:[%s]\n", cmd.Args)
-
-	//cmd.Stdout = os.Stdout
-	//cmd.Stderr = os.Stderr
-	//err := cmd.Run()
-	//if err != nil {
-	//	return err
-	//}
-
-	// 执行命令的逻辑
-	//fmt.Println("Executing command:", command)
-	return nil
-}
-
-// SplitMirrors 切割字符串
-func SplitMirrors(body string) {
-	temp := strings.Split(body, "\n")
-
-	// 去重
-	mirrorCtx = RemoveRepeatedElement(temp)
-}
-
 type list struct {
 	Repository string   `json:"Repository"`
 	Tags       []string `json:"Tags"`
 }
 
-func listTags(image string) *list {
+func main() {
+	body := httpclient("https://raw.githubusercontent.com/cnsync/image-sync/main/mirrors.txt")
+
+	mirrorCtx = strings.Split(body, "\n")
+
+	for _, cmd := range mirrorCtx {
+		srcRepo, srcTags := listTags(cmd)
+		if srcRepo == "" {
+			log.Println("Empty tags for command:", cmd)
+			return
+		}
+
+		srcRe, destRe := ImageContains(srcRepo, "docker.io/cnxyz")
+
+		_, destTag := listTags(destRe)
+
+		if destTag != nil {
+			finalTags = removeDuplicates(srcTags, destTag)
+
+			// 使用 sort.Slice 对数组进行排序
+			sort.Slice(finalTags, func(i, j int) bool {
+				return finalTags[i] < finalTags[j]
+			})
+		}
+
+		copyTags(srcRe, destRe)
+	}
+
+}
+
+func listTags(image string) (string, []string) {
 	var out bytes.Buffer
 
 	if image != "" {
@@ -115,28 +63,29 @@ func listTags(image string) *list {
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
-			log.Println(err)
-			return nil
+			log.Println("exec.Command 命令执行错误....")
+			return "", nil
 		}
 
 		var l list
 		err = json.Unmarshal(out.Bytes(), &l)
 		if err != nil {
+			log.Println("json.Unmarshal 转换错误....")
 			log.Fatal(err)
 		}
 
-		return &l
+		return l.Repository, l.Tags
 	}
 
-	return nil
+	return "", nil
 }
 
 // ImageContains 镜像名称处理
-func ImageContains(str *list, name string) (src, dest string, tags []string) {
-	beginIndex := strings.Index(str.Repository, "/")
-	b1 := str.Repository[beginIndex+1:]
+func ImageContains(repo string, name string) (src, dest string) {
+	beginIndex := strings.Index(repo, "/")
+	b1 := repo[beginIndex+1:]
 	b2 := strings.Replace(b1, "/", "-", -1)
-	return str.Repository, name + "/" + b2, str.Tags
+	return repo, name + "/" + b2
 }
 
 // httpclient http客户端
@@ -148,27 +97,45 @@ func httpclient(url string) string {
 	client := &http.Client{Transport: tr}
 	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Println("error:", err)
+		log.Println("error:", err)
 		return ""
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("error:", err)
+		log.Println("error:", err)
 		return ""
 	}
 	return string(body)
 }
 
-// RemoveRepeatedElement 通过map键的唯一性去重
-func RemoveRepeatedElement(s []string) []string {
-	result := make([]string, 0)
-	m := make(map[string]bool)
-	for _, v := range s {
-		if _, ok := m[v]; !ok {
-			result = append(result, v)
-			m[v] = true
+func removeDuplicates(left []string, right []string) []string {
+	set := make(map[string]bool)
+	for _, r := range right {
+		set[r] = true
+	}
+
+	var result []string
+	for _, l := range left {
+		if !set[l] {
+			result = append(result, l)
 		}
 	}
 	return result
+}
+
+func copyTags(srcRe, destRe string) {
+
+	for _, tag := range finalTags {
+
+		cmd := exec.Command("skopeo", "copy", "--insecure-policy", "--src-tls-verify=false", "--dest-tls-verify=false", "-q", "docker://"+srcRe+":"+tag, "docker://"+destRe+":"+tag)
+		log.Printf("CMD:[%s]\n", cmd.Args)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("Error executing command %s: %s\n", cmd.Args, err)
+		}
+	}
+
 }
